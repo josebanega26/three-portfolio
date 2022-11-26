4cc9e9110b29c183db15ac1f645def77fb50e2a7
José Banega <banegajose26@gmail.com>
2022-11-26 23:38:50 +0100

    feat: add laptop and inject iframe

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,12 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  Center,
+  ContactShadows,
+  Environment,
+  Float,
+  PresentationControls,
+  useGLTF,
+  Html,
+} from "@react-three/drei";
 import { Mesh } from "three";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
@@ -8,17 +16,57 @@ export const Experience = () => {
     performanceVisibility: false,
   });
 
+  const model = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+
+  console.log("model", model);
   return (
     <>
+      <Environment preset="city" />
       {performanceVisibility && <Perf position="top-left" />}
-      <OrbitControls makeDefault />
 
       {/* Color */}
-      <color args={["#201919"]} attach="background" />
+      <color args={["#7c3131"]} attach="background" />
+
+      <PresentationControls
+        global
+        rotation={[-0.15, 0.6, 0]}
+        polar={[-0.2, 0.35]}
+        azimuth={[-0.5, 0.75]}
+        config={{ mass: 3, tension: 200 }}
+        snap
+      >
+        <Float speed={0.2} rotationIntensity={0.3}>
+          {/* Screen light */}
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color="#efd6d6"
+            rotation={[0.1, Math.PI, 0]}
+            position={[-1.5, 0.55, -1.15]}
+          />
+          {/* Model */}
+          <primitive object={model.scene} position-y={-1.5} position-x={-1.5}>
+            <Html
+              transform
+              wrapperClass="iframe-container"
+              distanceFactor={1.2}
+              rotation-x={-0.256}
+              position={[0, 1.55, -1.4]}
+            >
+              <iframe src="https://josebanega.netlify.app/about" />
+            </Html>
+          </primitive>
+        </Float>
+      </PresentationControls>
 
       {/* Lights */}
       <ambientLight intensity={1} />
       <directionalLight intensity={1} castShadow position={[1, 2, 0]} />
+
+      <ContactShadows position-y={-1.8} opacity={0.5} scale={4} blur={2} />
     </>
   );
 };
